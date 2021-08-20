614cb87a7fbdaaa792dec258f61f1e2fb84a4ae3
enPlace <n.place@protonmail.com>
2021-08-20 12:26:30 -0300

    Move contact info into the contact component.
    Fix problem associated with having a state with nested objects and key value pairs by
    moving the contact info into the contact component. Iterate through the contactInfo state
    and generate Html and change the html to an editable form and input when clicked.

--- src/Components/Cv-components/Personal-info.js
+++ src/Components/Cv-components/Personal-info.js
@@ -3,25 +3,46 @@ import Name from "./PersonalInfo/Name";
 import Role from "./PersonalInfo/Role";
 import Contact from "./PersonalInfo/Contact";
 
+
 const PersonalInfo = () => {
-    const [persInfo, setPersInfo] = useState({
-        firstName: "First Name",
-        lastName: "Last Name",
-        role: "Current Role",
-        address1: "address 1",
-        address2: "address 2",
-        phone: "phone",
-        email: "email",
-        linkedIn: "LinkedIn"
-    })
- 
-    return ( 
-        <div className="personal-info">
-            <Name firstName = {persInfo.firstName} lastName = {persInfo.lastName}></Name>
-            <Role persInfo = {persInfo}></Role>
-            <Contact persInfo = {persInfo}></Contact>
-        </div>
-     );
-}
- 
+  const [isEditable, setIsEditable] = useState(false);
+  const [persInfo, setPersInfo] = useState([
+    {
+      firstName: "First Name",
+      lastName: "Last Name",
+      role: "Current Role",
+
+    },
+  ]);
+
+  const handleEditChange = (id) => {
+    return !isEditable ? setIsEditable(id) : false;
+  };
+  const handleChange = (e) => {
+
+    let newState = persInfo
+    console.log(newState)
+    newState[0][e.target.name].content = e.target.value;
+    console.log(newState)
+    setPersInfo(newState)
+    console.log(persInfo)
+  };
+
+  return (
+    <div className="personal-info">
+      <Name
+        firstName={persInfo[0].firstName}
+        lastName={persInfo[0].lastName}
+      ></Name>
+      <Role persInfo={persInfo}></Role>
+      <Contact
+        persInfo={persInfo}
+        isEditable={isEditable}
+        handleEditChange={handleEditChange}
+        handleChange={handleChange}
+      ></Contact>
+    </div>
+  );
+};
+
 export default PersonalInfo;
--- src/Components/Cv-components/PersonalInfo/Contact.js
+++ src/Components/Cv-components/PersonalInfo/Contact.js
@@ -1,13 +1,77 @@
-const Contact = ({persInfo}) => {
-    return ( 
-        <div className="contact">
-            <div className="contactInfo">{persInfo.address1}</div>
-            <div className="contactInfo">{persInfo.address2}</div>
-            <div className="contactInfo">{persInfo.phone}</div>
-            <div className="contactInfo">{persInfo.email}</div>
-            <div className="contactInfo">{persInfo.linkedIn}</div>
-        </div>
-     );
-}
- 
+import { useState } from "react";
+import LocationIcon from "./locationIcon.svg";
+import PhoneIcon from "./mobile-phone.svg";
+import EmailIcon from "./email2.svg";
+import LinkedIcon from "./linkedin.svg";
+
+const Contact = ({ handleEditChange, isEditable}) => {
+  /* for each of the items in contactInfo, I need to: 
+    1. check to see if it is editable or not. 
+    2. if !editable, display normal div. 
+         2a. Normal div should have: 
+          - a function that says, onClick, set this thing to editable. 
+          - a name that matches the id
+          - a key that matches the id 
+          
+*/
+  const [contactInfo, setContactInfo] = useState([
+    { text: "address 1", icon: LocationIcon, pos: 0, id: "address1" },
+    { text: "address 2", icon: LocationIcon, pos: 1, id: "address2" },
+    { text: "phone", icon: PhoneIcon, pos: 2, id: "phone" },
+    { text: "email", icon: EmailIcon, pos: 3, id: "email" },
+    { text: "LinkedIn", icon: LinkedIcon, pos: 4, id: "linkedIn" },
+  ]);
+
+  const handleChange = (e)=>{
+   const list = contactInfo.filter(item=> item.id !== e.target.name)
+   const target = contactInfo.filter(item =>item.id === e.target.name)
+   target[0].text = e.target.value
+   list.splice(target.[0].pos, 0, target[0])
+   console.log(list)
+   setContactInfo(list)
+  }
+  return (
+    <div className="contact">
+      {contactInfo.map((item) => {
+        if (isEditable !== item.id) {
+          return (
+            <div key={item.id} className="contactItem">
+              <div
+                onClick={() => {
+                  handleEditChange(item.id);
+                }}
+                name={item.id}
+                className="contactItemName"
+              >
+                {item.text}
+              </div>
+              <img src={item.icon} alt="" />
+            </div>
+          );
+        } else {
+          return (
+            <div className="contactItem">
+              <form
+                action=""
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleEditChange();
+                }}
+              >
+                <input
+                  name={item.id}
+                  type="text"
+                  value={item.text}
+                  onChange={handleChange}
+                />
+              </form>
+              <img src={item.icon} alt="" />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
 export default Contact;
